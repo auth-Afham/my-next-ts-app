3f0d87fce98855b47b9a29724009f69aae1a7adf
Your Name <your.email@example.com>
2025-03-23 09:47:42 +0800

    remove experimental features and delete unused page components

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    appDir: true,
-  },
-  devIndicators: {
-    buildActivity: false, // Disables the loading spinner
-  },
 };
 
 export default nextConfig;
